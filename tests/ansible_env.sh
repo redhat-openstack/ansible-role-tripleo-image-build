efc771b05937439d13fff6cc20458d10cd3eeeb7
Matt Young <myoung@redhat.com>
2016-04-17 00:17:38 -0400

    WIP: initial whack at adding simple self test
    Change-Id: Iba467a2f8fa5ed91f1ad9e6ac949a0828389b5dc

---
+++ tests/ansible_env.sh
@@ -0,0 +1,25 @@
+export ANSIBLE_GATHERING=smart
+export ANSIBLE_COMMAND_WARNINGS=False
+export ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_FORCE_COLOR=1
+export ANSIBLE_INVENTORY=$OPT_WORKDIR/hosts
+export SSH_CONFIG=$OPT_WORKDIR/ssh.config.ansible
+export ANSIBLE_SSH_ARGS="-F ${SSH_CONFIG}"
+
+# to find add-inventory.virthost
+export ANSIBLE_ROLES_PATH=$(pwd)
+
+# to find role: parts/kvm, parts/libvirt.  for some reason when pulled in directly by build_default_images.yml they need to be found this way
+export ANSIBLE_ROLES_PATH=$ANSIBLE_ROLES_PATH:$(pwd)/..
+
+# to find role: ansible-role-tripleo-image-build itself
+export ANSIBLE_ROLES_PATH=$ANSIBLE_ROLES_PATH:$(pwd)/../..
+
+
+# TODO: fix
+export ANSIBLE_CALLBACK_PLUGINS=/home/matyoung/ci/ansible-diag/plugins/callback/profile_timeline
+export ANSIBLE_CALLBACK_WHITELIST=profile_timeline
+export PROFILE_TASKS_TIMELINE_SUMMARY=1
+
+
+
