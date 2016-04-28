435b8c0a866debe6e691907e5ffe53d298b3df4f
John Trowbridge <trown@redhat.com>
2016-04-28 16:59:32 -0400

    WIP Create tripleo_build_images module
    This is still pretty WIP. However, the module does work
    modulo this todo list.
    TODO:
      - make sure tripleo_common is installed on the virthost
      - patch tripleo_common not to spew to stderr/stdout
      - Add documentation to tripleo_build_images.py
    
    I made the yaml configs into templates.
    
    Resolves: https://github.com/redhat-openstack/ansible-role-tripleo-image-build/issues/17
    Change-Id: I37f122782b03ba7a5dfb2aa52ad6f9b657d2d4d2

---
+++ library/tripleo_build_images.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# coding: utf-8 -*-
+
+# this module is based directly on the script of the
+# same name in openstack/tripleo-common/scripts
+
+DOCUMENTATION = '''
+---
+module: tripleo_build_images
+version_added: "2.0.1"
+short_description: Build a TripleO overcloud image from a list of yaml definitions
+description:
+   - Build a TripleO overcloud image from a list of yaml definitions.
+'''
+from tripleo_common.image.build import ImageBuildManager
+
+def tripleo_build_images(config_files, output_directory, skip):
+
+    try:
+        # (trown) the tripleo-common dib builder spews to stdout/stderr,
+        # which makes the results file not work for async.
+        # We have to fix that in tripleo-common... I tried various means to
+        # use mock as a hack, but no luck. The patch to tripleo-common is pretty
+        # straight-forward though.
+        # just need to patch in a log file to the subprocess.check_call() here:
+        # https://github.com/openstack/tripleo-common/blob/master/tripleo_common/image/image_builder.py#L89
+        manager = ImageBuildManager(config_files,
+                                    output_directory=output_directory,
+                                    skip=skip)
+        manager.build()
+    except Exception as e:
+        return ('Fail', e.args)
+
+    return ('Success', '')
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            config_files=dict(required=True, type='list'),
+            output_directory=dict(required=True),
+            skip=dict(required=False, default=False, type='bool')
+        )
+    )
+    result = tripleo_build_images(module.params["config_files"],
+                                  module.params["output_directory"],
+                                  module.params["skip"])
+
+    if result[0] == 'Fail':
+        module.fail_json(msg=' '.join(result[1]))
+    else:
+        module.exit_json(changed=True)
+
+# see http://docs.ansible.com/developing_modules.html#common-module-boilerplate
+from ansible.module_utils.basic import *
+
+if __name__ == "__main__":
+    main()
