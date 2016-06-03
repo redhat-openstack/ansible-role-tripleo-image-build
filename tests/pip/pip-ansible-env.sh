8fb27a0b16123ec9229e60944b4c1e25b988da33
Matt Young <myoung@redhat.com>
2016-06-02 20:55:36 -0400

    Remove embedded fork of parts/libvirt and parts/kvm
    Addresses: https://github.com/redhat-openstack/ansible-role-tripleo-image-build/issues/2
    
    This commit removes embedded fork from oooq
    pointing instead to new shared parts repo:
    
    https://github.com/redhat-openstack/ansible-role-tripleo-parts
    
    Change-Id: Ibc1eba8af38adae92f34129fc7e49cb4745123ee

--- tests/pip/pip-ansible-env.sh
+++ tests/pip/pip-ansible-env.sh
@@ -9,11 +9,11 @@ export ANSIBLE_SSH_ARGS="-F ${SSH_CONFIG}"
 # to find roles/add-inventory-virthost
 export ANSIBLE_ROLES_PATH=$(pwd)
 
-# to find parts/kvm, parts/libvirt.
-export ANSIBLE_ROLES_PATH=$ANSIBLE_ROLES_PATH:$(pwd)/../..
-
-# to find the artib itself: ansible-role-tripleo-image-build itself
+# to find artib itself
 export ANSIBLE_ROLES_PATH=$ANSIBLE_ROLES_PATH:$(pwd)/../../..
+
+# to find parts/kvm, parts/libvirt in venv
+export ANSIBLE_ROLES_PATH=$ANSIBLE_ROLES_PATH:$VIRTUAL_ENV/usr/local/share
 
 export ANSIBLE_CALLBACK_PLUGINS=test_plugins/callback/profile_tasks
 export ANSIBLE_CALLBACK_WHITELIST=profile_tasks
